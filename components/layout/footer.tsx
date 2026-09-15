2361c72df98f69aede76851a0636afbc3e22c839
Alfredo R. Pabon <alfredrafael@gmail.com>
2026-09-14 22:02:54 -0500

    Add Fb icon

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -3,8 +3,8 @@ import { Container } from "@/components/craft";
 import { siteConfig } from "@/site.config";
 import Link from "next/link";
 import Image from "next/image";
-import { DAC_Icon } from "../icons/DAC_Icon";
 import { InstagramIcon } from "../icons/InstagramIcon";
+import { FacebookIcon } from "../icons/FacebookIcon";
 
 export function Footer() {
   return (
@@ -12,28 +12,32 @@ export function Footer() {
       <div className="max-w-[90%] mx-auto mt-12">
         <hr />
       </div>
-      <div className="max-w-6xl mx-auto flex flex-col h-24 md:flex-row md:gap-2 gap-6 justify-between md:items-center px-4 md:px-0 py-4">
+      <div className="max-w-6xl mx-auto flex flex-row h-24 gap-2 justify-between items-center px-4 md:px-0 py-4">
         <div>
           <div className="flex items-center gap-4">
             <Link
               className="flex items-center gap-4 transition-all hover:opacity-75"
               href="/"
             >
-              <h3 className="sr-only">{siteConfig.site_name}</h3>
               <Image
                 src="https://www.alfredorafael.com/wp-content/uploads/2026/09/HabershamRE_logo-scaled.png"
                 alt="Site Logo"
                 width={100}
                 height={100}
               />
-              <h2 className="hidden text-sm">{siteConfig.site_name}</h2>
             </Link>
           </div>
         </div>
-        <div className="items-center gap-1 hidden sm:flex">
-          <span className="text-sm text-mutted">Follow us on</span>
+        <div className="items-center gap-5 flex justify-end border-gray-200">
           <a
-            href="https://www.instagram.com/dac_architects_llc/"
+            href="https://www.facebook.com/habershamrealestate/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FacebookIcon width={32} height={32} />
+          </a>
+          <a
+            href="https://www.instagram.com/habershamrealestate/"
             target="_blank"
             rel="noopener noreferrer"
           >
